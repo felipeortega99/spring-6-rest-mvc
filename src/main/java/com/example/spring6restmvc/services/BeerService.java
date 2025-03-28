b65cfc2f0b5d30f911b0736261519073d85a6417
felipeortega99 <felipeorcas@gmail.com>
2025-03-28 16:10:24 -0600

    Adding builder example

---
+++ src/main/java/com/example/spring6restmvc/services/BeerService.java
@@ -0,0 +1,10 @@
+package com.example.spring6restmvc.services;
+
+import com.example.spring6restmvc.model.Beer;
+
+import java.util.UUID;
+
+public interface BeerService {
+
+    Beer getBeerById(UUID id);
+}
---
+++ src/main/java/com/example/spring6restmvc/services/BeerServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.spring6restmvc.services;
+
+import com.example.spring6restmvc.model.Beer;
+import com.example.spring6restmvc.model.BeerStyle;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class BeerServiceImpl implements BeerService {
+    @Override
+    public Beer getBeerById(UUID id) {
+        return Beer.builder()
+                .id(id)
+                .version(1)
+                .beerName("Vaquita Marina")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("123456789")
+                .price(new BigDecimal("45.00"))
+                .quantityOnHand(122)
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+    }
+}
