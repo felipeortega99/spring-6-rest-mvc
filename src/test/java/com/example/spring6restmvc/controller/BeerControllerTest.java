a518d173da9bfb09fcbca68e6930662ff2bee707
felipeortega99 <felipeorcas@gmail.com>
2025-03-28 17:00:03 -0600

    Adding lombok logging example

---
+++ src/test/java/com/example/spring6restmvc/controller/BeerControllerTest.java
@@ -0,0 +1,21 @@
+package com.example.spring6restmvc.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BeerControllerTest {
+
+    @Autowired
+    BeerController controller;
+
+    @Test
+    void getBeerById() {
+        System.out.println(controller.getBeerById(UUID.randomUUID()));
+    }
+}
