0443540b6cdf6b0abf2c961c5e96c14d4fd5b437
felipeortega99 <felipeorcas@gmail.com>
2025-04-01 15:59:02 -0600

    Adding http post example

--- src/main/java/com/example/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/example/spring6restmvc/controller/BeerController.java
@@ -4,8 +4,11 @@ import com.example.spring6restmvc.model.Beer;
 import com.example.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +33,12 @@ public class BeerController {
         log.debug("Get Beer by Id - in controller");
 
         return beerService.getBeerById(beerId);
+    }
+
+    @PostMapping
+//    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity handlePost(@RequestBody Beer beer) {
+        Beer savedBeer = beerService.saveNewBeer(beer);
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 }
