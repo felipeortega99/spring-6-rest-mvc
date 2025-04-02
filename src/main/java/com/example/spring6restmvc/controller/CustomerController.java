8cdf46e264c226501c2838ebe75c851f039cfb2c
felipeortega99 <felipeorcas@gmail.com>
2025-04-01 19:34:09 -0600

    Adding delete customer endpoint

--- src/main/java/com/example/spring6restmvc/controller/CustomerController.java
+++ src/main/java/com/example/spring6restmvc/controller/CustomerController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -51,6 +52,12 @@ public class CustomerController {
     public ResponseEntity updateById(@PathVariable("customerId") UUID customerId, @RequestBody Customer customer) {
         customerService.updateCustomerById(customerId, customer);
 
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("{customerId}")
+    public ResponseEntity deleteById(@PathVariable("customerId") UUID customerId) {
+        customerService.deleteById(customerId);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/example/spring6restmvc/services/CustomerServiceImpl.java
+++ src/main/java/com/example/spring6restmvc/services/CustomerServiceImpl.java
@@ -81,4 +81,9 @@ public class CustomerServiceImpl implements CustomerService {
         existingCustomer.setName(customer.getName());
         customerMap.put(existingCustomer.getId(), existingCustomer);
     }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        customerMap.remove(customerId);
+    }
 }
