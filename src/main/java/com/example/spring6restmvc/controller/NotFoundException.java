a5441364fb8b040945221773d5d13bc77b5195b0
felipeortega99 <felipeorcas@gmail.com>
2025-04-16 18:23:56 -0600

    Adding custom exception

---
+++ src/main/java/com/example/spring6restmvc/controller/NotFoundException.java
@@ -0,0 +1,23 @@
+package com.example.spring6restmvc.controller;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException() {
+    }
+
+    public NotFoundException(final String message) {
+        super(message);
+    }
+
+    public NotFoundException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotFoundException(final Throwable cause) {
+        super(cause);
+    }
+
+    public NotFoundException(final String message, final Throwable cause, final boolean enableSuppression, final boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
