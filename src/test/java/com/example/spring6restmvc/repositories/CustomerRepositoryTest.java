791f46b625bb96745e3e6191726ec971d2188b6a
felipeortega99 <felipeorcas@gmail.com>
2025-05-19 14:31:07 -0600

    Adding JPA tests

---
+++ src/test/java/com/example/spring6restmvc/repositories/CustomerRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.example.spring6restmvc.repositories;
+
+import com.example.spring6restmvc.entities.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class CustomerRepositoryTest {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void testSaveCustomer() {
+        Customer customer = customerRepository.save(Customer.builder()
+                        .name("John Doe")
+                .build());
+
+        assertThat(customer.getId()).isNotNull();
+    }
+}
