06f945ca5490de723bf3945b4bacbd8aee58e06c
felipeortega99 <felipeorcas@gmail.com>
2025-05-19 16:11:40 -0600

    Testing controller list operation

---
+++ src/test/java/com/example/spring6restmvc/controller/BeerControllerIT.java
@@ -0,0 +1,41 @@
+package com.example.spring6restmvc.controller;
+
+import com.example.spring6restmvc.model.BeerDTO;
+import com.example.spring6restmvc.repositories.BeerRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class BeerControllerIT {
+    @Autowired
+    BeerController controller;
+
+    @Autowired
+    BeerRepository repository;
+    @Autowired
+    private BeerRepository beerRepository;
+
+    @Test
+    void testListBeers() {
+        List<BeerDTO> dtos = controller.listBeers();
+
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyList() {
+        beerRepository.deleteAll();
+        List<BeerDTO> dtos = controller.listBeers();
+
+        assertThat(dtos.size()).isEqualTo(0);
+    }
+}
