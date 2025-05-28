8648b16223c0545604737bf309dfb39ec0fa2a47
felipeortega99 <felipeorcas@gmail.com>
2025-05-27 19:04:46 -0600

    Impl update of beer

--- src/test/java/com/example/spring6restmvc/controller/BeerControllerIT.java
+++ src/test/java/com/example/spring6restmvc/controller/BeerControllerIT.java
@@ -1,6 +1,7 @@
 package com.example.spring6restmvc.controller;
 
 import com.example.spring6restmvc.entities.Beer;
+import com.example.spring6restmvc.mappers.BeerMapper;
 import com.example.spring6restmvc.model.BeerDTO;
 import com.example.spring6restmvc.repositories.BeerRepository;
 import jakarta.transaction.Transactional;
@@ -24,6 +25,25 @@ class BeerControllerIT {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Autowired
+    BeerMapper beerMapper;
+
+    @Test
+    void updateExistingBeer() {
+        Beer beer = beerRepository.findAll().get(0);
+        BeerDTO beerDTO = beerMapper.beerToBeerDTO(beer);
+        beerDTO.setId(null);
+        beerDTO.setVersion(null);
+        final String beerName = "UPDATED";
+        beerDTO.setBeerName(beerName);
+
+        ResponseEntity responseEntity = controller.updateById(beer.getId(), beerDTO);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(204));
+
+        Beer updatedBeer = beerRepository.findById(beer.getId()).get();
+        assertThat(updatedBeer.getBeerName()).isEqualTo(beerName);
+    }
 
     @Rollback
     @Transactional
